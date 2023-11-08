3007121967da63ab33e84f2d3232b4f942749f23
Techspace-2020 <rakeshcp85@gmail.com>
2023-11-08 13:17:40 +0530

    Revert "Added new packages and java classes"
    This reverts commit 4053acaffc605d370fbec96946972d3051a0d4ef.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Java-programs
-This repository contains the java program examples from basic to intermediate to learn and understand the concepts 
--- src/com/practice/string_problem/RemoveSpaces.java
+++
@@ -1,8 +0,0 @@
-package com.practice.string_problem;
-
-public class RemoveSpaces {
-    public static void main(String[] args) {
-        String str = "Remove white spaces in the line";
-        System.out.println(str.replaceAll("\\s",""));
-    }
-}
