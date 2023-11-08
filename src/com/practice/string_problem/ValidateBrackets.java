51367e1beb27c9418564b09e00697f52ba47db73
Techspace-2020 <rakeshcp85@gmail.com>
2023-11-08 18:49:26 +0530

    Rakesh: minor changes

--- src/com/practice/string_problem/ValidateBrackets.java
+++ src/com/practice/string_problem/ValidateBrackets.java
@@ -4,14 +4,16 @@ import java.util.Stack;
 
 public class ValidateBrackets {
     public static void main(String[] args) {
-        String str ="(){}[]";
+        String str = "(){}[]";
         Boolean result = ValidateString(str);
         System.out.println("Validated brackets:"+ result);
     }
+
     private static Boolean ValidateString(String str) {
         Stack<Character> stack = new Stack();
         int i;
-        for (i = 0; i < str.length() - 1; i++) ;
+
+        for (i = 0; i < str.length() -1; i++) ;
         char ch = str.charAt(i);
 
         if (ch == ')')
